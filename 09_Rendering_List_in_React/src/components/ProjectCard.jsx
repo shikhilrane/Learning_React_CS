2dcc0d054c7840408f4cf6dcfce75dd1a6c9b83c
Shikhil Rane <shikhilrane04@gmail.com>
2026-03-16 20:21:22 +0530

    added jsx component and list

---
+++ 09_Rendering_List_in_React/src/components/ProjectCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import uber from '../assets/Uber_logo_2018.svg.png'
+import './ProjectCard.css'
+import githubIcon from '../assets/25231.png'
+
+function ProjectCard({title,description,imgUrl,gitHubLink}) {
+  return (
+    <div className='project-card'>
+        <div className="img-wrapper">
+            <img src={imgUrl} alt=""/>
+        </div>
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <a href={gitHubLink} className='img-button' target='_blank'>
+          <img src={githubIcon} alt={"Project Icon"+title}/> 
+          <span>View on GitHub</span> 
+          </a>
+    </div>
+  )
+}
+
+export default ProjectCard
